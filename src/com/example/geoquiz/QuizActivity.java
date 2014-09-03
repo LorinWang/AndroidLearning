50e3ddff614518781ad9cddc5b33ea51da15fcca
WangYiming <wangyimingfp@gmail.com>
2014-09-03 22:26:57 +0800

    complete interactive between activities.

--- src/com/example/geoquiz/QuizActivity.java
+++ src/com/example/geoquiz/QuizActivity.java
@@ -1,41 +1,133 @@
 package com.example.geoquiz;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
 
+	private static final String TAG="QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private Button mCheatButton;
 	
+	private TextView mQuestionTextView;
+
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true), };
+
+	private int mCurrentIndex = 0;
+
+	private boolean mIsCheater;
+	
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(data==null){
+			return;
+		}
+		mIsCheater=data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+	}
+	
+
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+		int messageResId = 0;
+		
+		if(mIsCheater){
+			messageResId=R.string.judgment_toast;
+		}else{
+			if (userPressedTrue == answerIsTrue) {
+				messageResId = R.string.correct_toast;
+			} else {
+				messageResId = R.string.incorrect_toast;
+			}
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onStart() called");
+		
 		setContentView(R.layout.activity_quiz);
-		
-		mTrueButton=(Button) findViewById(R.id.true_button);
+
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
-		
-		mFalseButton=(Button) findViewById(R.id.false_button);
+
+		mFalseButton = (Button) findViewById(R.id.false_button);
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
+			}
+		});
+
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				mIsCheater=false;
+				updateQuestion();
+			}
+		});
+		if(savedInstanceState!=null)
+		{
+			mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+		}
+		
+		mCheatButton=(Button) findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i=new Intent(QuizActivity.this,CheatActivity.class);
+				boolean answerIsTrue=mQuestionBank[mCurrentIndex].isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+				//startActivity(i);
+				startActivityForResult(i, 0);
 			}
 		});
 		
+		updateQuestion();
+	}
+
+	
+	@Override
+	protected void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		Log.i(TAG, "onSaveInstanceState");
+		savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
 	}
 
 	@Override
@@ -56,4 +148,34 @@ public class QuizActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+	
+	
 }
